79075d87a4ef0a41817a66ad7af4479aef924341
devmashum <dev.mashum@gmail.com>
2023-09-06 21:41:13 +0200

    new commit

--- array.methods.js
+++ array.methods.js
@@ -3,17 +3,26 @@ const products = [
     { name: 'phone', price: 7000, brand: 'iphone', color: 'golden' },
     { name: 'watch', price: 3000, brand: 'casio', color: 'yellow' },
     { name: 'sunglass', price: 300, brand: 'ribon', color: 'black' },
-]
+];
 
-// const brands = products.map(product => product.brand);
-// console.log(brands);
 
-// products.forEach(product => console.log(product.color));
-// products.forEach(product => console.log(product.price));
-// products.forEach(product => console.log(product.brand));
+
+const brands = products.map(product => product.brand);
+console.log(brands);
+
+products.forEach(product => console.log(product.color));
+products.forEach(product => console.log(product.price));
+products.forEach(product => console.log(product.brand));
 
 const cheap = products.filter(product => product.price <= 3000);
 console.log(cheap);
 
 const color = products.filter(product => product.color === 'black');
 console.log(color);
+
+
+
+const findValue = products.find(product => product.price === 300);
+console.log(findValue);
+const findValue1 = products.filter(product => product.price <= 4000);
+console.log(findValue1);
